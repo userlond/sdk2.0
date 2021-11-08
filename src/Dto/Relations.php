34ae98a95a1609d0e45901493cd4992b0b141399
Andrey Muriy <andrey.muriy@gmail.com>
2021-11-08 16:04:09 +0300

    Relation DTO has been modified

--- src/Dto/Relations.php
+++ src/Dto/Relations.php
@@ -10,14 +10,61 @@ class Relations
 {
     /**
      * Тип сущности, связанной с заказом
+     *
      * @Type("string")
      * @var string
      */
     public $type;
 
     /**
+     * Идентификатор сущности, связанной с заказом
+     *
      * @Type("string")
      * @var string
      */
     public $uuid;
+
+    /**
+     * Ссылка на скачивание печатной формы в статусе "Сформирован", только для type = waybill, barcode
+     *
+     * @Type("string")
+     * @var string|null
+     */
+    public $url;
+
+    /**
+     * Номер заказа СДЭК
+     * Может возвращаться для return_order, direct_order, reverse_order
+     *
+     * @Type("string")
+     * @var string|null
+     */
+    public $cdek_number;
+
+    /**
+     * Дата доставки, согласованная с получателем
+     * Только для типа delivery
+     *
+     * @Type("string")
+     * @var string|null
+     */
+    public $date;
+
+    /**
+     * Время начала ожидания курьера (согласованное с получателем)
+     * Только для типа delivery
+     *
+     * @Type("string")
+     * @var string|null
+     */
+    public $time_from;
+
+    /**
+     * Время окончания ожидания курьера (согласованное с получателем)
+     * Только для типа delivery
+     *
+     * @Type("string")
+     * @var string|null
+     */
+    public $time_to;
 }
